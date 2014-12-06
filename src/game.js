4964844f4e2eaba54c7fff4fa78b2d4195750d99
Shelo <marjara35@gmail.com>
2014-12-06 09:28:45 -0400

    Add context menu to Hierarchy.

--- src/game.js
+++ src/game.js
@@ -5,17 +5,20 @@ var colors = [
 /*
  * add a truck to the final of the hierarchy.
  */
-function addTruck(color, locked) {
+function addTruck(color, byDefault) {
     // create DOM elements.
-    var truck = $('<div class="truck">');
-    var truckColor = $('<div class="truck-color">');
-    var truckCommands = $('<div class="truck-commands">');
+    var container       = $('<div class="container">');
+    var truck           = $('<table class="truck">');
+    var truckColor      = $('<td class="truck-color">');
+    var truckCommands   = $('<td class="truck-commands">');
 
     // modify truck's color
     truckColor.css('background-color', color);
     truckColor.attr('data-color', color);
-    if(locked)
-        truckColor.html('<img src="img/lock.png">');
+    if(byDefault)
+        truck.attr('data-default', 'true');
+    else
+        truckColor.click(truckColorClick);
 
     // append children to the truck.
     truck.append(truckColor);
@@ -28,7 +31,14 @@ function addTruck(color, locked) {
         truckCommandsClick($(this));
     });
 
-    $('#trucks').append(truck);
+    truckColor.on('contextmenu', showContextMenu);
+
+    // add to container and hide.
+    container.append(truck);
+    container.hide();
+
+    $('#trucks').append(container);
+    container.slideDown();
 }
 
 /*
@@ -36,6 +46,9 @@ function addTruck(color, locked) {
  * selected command, from the command nav, if so, we add it to the list.
  */
 function truckCommandsClick(target) {
+    if(target.parent().attr('data-locked') == 'true')
+        return;
+
     $('.nav-command').each(function() {
         if($(this).hasClass('selected')) {
             var img = $('<img>');
@@ -45,6 +58,58 @@ function truckCommandsClick(target) {
             target.append(img);
         }
     });
+}
+
+function truckColorClick() {
+    var color = getUnusedColor();
+    $(this).css('background-color', color);
+    $(this).attr('data-color', color);
+}
+
+function getUnusedColor() {
+    var available = colors.slice();
+
+    $('.truck-color').each(function() {
+        var color = $(this).attr('data-color');
+        var index = available.indexOf(color);
+        available.splice(index, 1);
+    });
+
+    if(available.length > 0)
+        return available[Math.floor(Math.random() * available.length)];
+    else
+        return false;
+}
+
+function showContextMenu(event) {
+    var color   = event.currentTarget.attributes['data-color'].value;
+    var pos     = event.currentTarget.offsetParent.offsetTop;
+    var index   = -1;
+    var dom;
+
+    // find the element clicked.
+    var found = false;
+    $('.truck').each(function() {
+        if(! found) {
+            index++;
+            if($(this).children('.truck-color').attr('data-color') == color) {
+                dom = $(this);
+                found = true;
+            }
+        }
+    });
+
+    // something really weird just happened.
+    if(dom == undefined) return false;
+
+    // hide delete button if it is a default truck.
+    if(dom.attr('data-default') == 'true')
+        $('#context-delete').css('display', 'none');
+    else
+        $('#context-delete').css('display', 'block');
+
+    $('#context').attr('data-index', index).css('top', pos).fadeIn();
+    return false;
 }
 
 /*
@@ -63,45 +128,89 @@ var navCommandDrag = {
 };
 
 /*
+ * Actions of the context menu.
+ */
+var contextMenuActions = {
+    'lock / unlock': function(index) {
+        var element = $('.truck:eq(' + index + ')');
+
+        if(element.attr('data-locked') == 'true') {
+            element.attr('data-locked', 'false');
+        } else {
+            element.attr('data-locked', 'true');
+        }
+
+        // avoid inconsistencies.
+        element.children('.truck-color').html('');
+        if(element.attr('data-locked') == 'true')
+            element.children('.truck-color').html('<img src="img/lock.png">');
+    },
+
+    'delete': function(index) {
+        $('.truck:eq(' + index + ')').parent().slideUp(function() {
+            $(this).remove();
+        });
+    }
+};
+
+/*
  * Used when something has been dropped inside a command section.
  */
 var truckCommandsDrop = {
     drop: function(event, ui) {
+        if($(this).parent().attr('data-locked') == 'true')
+            return;
+
         if(ui.draggable.hasClass('nav-command')) {
             var img = $('<img>');
             img.attr('src', ui.draggable.attr('src'));
             img.attr('data-id', ui.draggable.attr('id'));
             $(this).append(img);
+
+            // delete this command when user clicks the secondary button.
+            img.bind('contextmenu', function() {
+                $(this).remove();
+                return false;
+            });
         }
     }
 };
 
 $(document).ready(function() {
     $(document).disableSelection();
-    addTruck(colors[0]);
-
-    $('.nav-command').draggable(navCommandDrag);
-    $('.nav-command').click(function() {
-        var command = $(this);
-
-        $('.nav-command').each(function() {
-            if($(this).attr('src') != command.attr('src'))
-                $(this).removeClass('selected');
-        });
-
-        $(this).toggleClass('selected');
+    $(document).click(function() {
+        $('#context').fadeOut();
     });
 
-    $('#plus-button').click(function() {
-        var available = colors.slice();
+    $('#context').click(function(e) {
+        e.stopPropagation();
+    });
 
-        $('.truck-color').each(function() {
-            var color = $(this).attr('data-color');
-            var index = available.indexOf(color);
-            available.splice(index, 1);
+    $('#context > a').click(function() {
+        if($(this).text() in contextMenuActions)
+            contextMenuActions[$(this).text()](parseInt($(this).parent().attr('data-index')));
+
+        $('#context').fadeOut();
+    });
+
+    addTruck(colors[0], true);
+
+    $('.nav-command')
+        .draggable(navCommandDrag)
+        .click(function() {
+            var command = $(this);
+
+            $('.nav-command').each(function() {
+                if($(this).attr('src') != command.attr('src'))
+                    $(this).removeClass('selected');
+            });
+
+            $(this).toggleClass('selected');
         });
 
-        if(available.length > 0)
-            addTruck(available[Math.floor(Math.random() * available.length)], false);
+    $('#plus-button').click(function() {
+        var color = getUnusedColor();
+        if(color != false)
+            addTruck(color, false);
     });
 });
